bd72f6b0db2b089e77d1bef32e581ccc19fdc36f
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-06 10:06:40 +0000

    UPDATE(pusher.sh).

--- pusher.sh
+++ pusher.sh
@@ -1,5 +1,5 @@
 echo $(git status --porcelain  | awk 'match($1, ""){print $2}' )
-for file in $(git ls-files -m);
+for file in $(git status --porcelain  | awk 'match($1, ""){print $2}' );
     do
         git add $file 2> /dev/null
         git commit -m "UPDATE($file)."
