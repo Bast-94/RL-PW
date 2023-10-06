649c73de653d9add2642dee4993d87851fed87a3
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-06 10:02:57 +0000

    UPDATE(pusher.sh).

--- pusher.sh
+++ pusher.sh
@@ -1,9 +1,4 @@
-for file in $(git ls-files -o);
-    do
-        echo add $file
-        git add $file 2> /dev/null
-        git commit -m "ADD($file)."
-    done
+
 for file in $(git ls-files -m);
     do
         git add $file 2> /dev/null
