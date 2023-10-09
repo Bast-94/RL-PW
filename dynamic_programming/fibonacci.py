1b6f800c83bd7332c65d6ebdad834e80ac699a52
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-09 11:53:19 +0000

    UPDATE(dynamic_programming/fibonacci.py).

---
+++ dynamic_programming/fibonacci.py
@@ -0,0 +1,54 @@
+"""
+Partie 2 - Programmation dynamique
+==================================
+
+Rappel: la programmation dynamique est une technique algorithmique qui
+permet de résoudre des problèmes en les décomposant en sous-problèmes
+plus petits, et en mémorisant les solutions de ces sous-problèmes pour
+éviter de les recalculer plusieurs fois.
+"""
+
+# Exercice 1: Fibonacci
+# ----------------------
+# La suite de Fibonacci est définie par:
+#   F(0) = 0
+#   F(1) = 1
+#   F(n) = F(n-1) + F(n-2) pour n >= 2
+#
+# Ecrire une fonction qui calcule F(n) pour un n donné.
+# Indice: la fonction doit être récursive.
+
+
+def fibonacci(n: int) -> int:
+    """
+    Calcule le n-ième terme de la suite de Fibonacci.
+    """
+    # BEGIN SOLUTION
+    if n in [0, 1]:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+    # END SOLUTION
+
+
+# Exercice 2: Fibonacci avec mémorisation
+# ---------------------------------------
+# Ecrire une fonction qui calcule F(n) pour un n donné, en mémorisant
+# les résultats intermédiaires pour éviter de les recalculer plusieurs
+# fois.
+# Indice: la fonction doit être récursive.
+
+
+def fibonacci_memo(n: int) -> int:
+    """
+    Calcule le n-ième terme de la suite de Fibonacci, en mémorisant les
+    résultats intermédiaires.
+    """
+
+    # BEGIN SOLUTION
+    def fibo_rec(n: int, a1: int, a2: int):
+        if n == 0:
+            return a1
+        return fibo_rec(n - 1, a2, a1 + a2)
+
+    return fibo_rec(n, 0, 1)
+    # END SOLUTION
