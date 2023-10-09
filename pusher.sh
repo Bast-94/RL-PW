71af37e8abab1683eef663dffa8d51ed9aa03327
Bast-94 <bh.69@live.fr>
2023-10-09 22:19:28 +0200

    UPDATE(pusher.sh) at 10/09/23-22:19:28

--- pusher.sh
+++ pusher.sh
@@ -2,6 +2,6 @@ echo $(git status --porcelain  | awk 'match($1, ""){print $2}' )
 for file in $(git status --porcelain  | awk 'match($1, ""){print $2}' );
     do
         git add $file 2> /dev/null
-        git commit -m "UPDATE($file)."
+        git commit -m "UPDATE($file) at $(date +%m/%d/%y-%H:%M:%S)"
     done
 # git push
