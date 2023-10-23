dcb7ae27e64b505c7c6cc1897909e62758f2f218
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-23 10:27:13 +0000

    UPDATE(pusher.sh): Changing commiting porcess.

--- pusher.sh
+++ pusher.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 pusher() {
-    git add . 2> /dev/null
+    
     echo $(git status --porcelain  | awk 'match($1, ""){print $2}' )
     for file in $(git status --porcelain  | awk 'match($1, ""){print $2}' );
         do
-            
+            git add $file 2> /dev/null
             echo "Commiting $file"
             if [ -z "$1" ]; then
                 read msg
             fi
-            echo $msg > empty.txt
             
+
             git commit -m "UPDATE($file): $msg"
         done
 }
