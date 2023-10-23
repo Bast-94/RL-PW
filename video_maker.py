8d1145640815ef2037d1149b76637048a9647b3a
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-23 15:56:45 +0000

    UPDATE(video_maker.py): Beginning of env render implementation.

--- video_maker.py
+++ video_maker.py
@@ -1,7 +1,16 @@
+import argparse
 import glob
 
 import cv2
+import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
+from qlearning import QLearningAgent
+from sarsa import SarsaAgent
+from taxi import play_and_train
+env = gym.make("Taxi-v3", render_mode="rgb_array")
+n_actions = env.action_space.n
+print(env.render())
 fig, ax = plt.subplots()
