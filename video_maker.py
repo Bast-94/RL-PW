d316c4e4525e71855a8ce57ba5e8f85a1c86a59e
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-24 20:44:12 +0000

    UPDATE(video_maker.py): Add test for every agents.

--- video_maker.py
+++ video_maker.py
@@ -1,28 +1,48 @@
 import cv2
 import gymnasium as gym
 import numpy as np
-
-"""env = gym.make("Taxi-v3", render_mode="rgb_array")
+from taxi import play_and_train
+from qlearning import QLearningAgent
+from qlearning_eps_scheduling import QLearningAgentEpsScheduling
+from sarsa import SarsaAgent
+from tqdm import tqdm
+from train_agent import train
+env = gym.make("Taxi-v3", render_mode="rgb_array")
 n_actions = env.action_space.n
-env.reset()
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-height, width = 600, 400
-video = cv2.VideoWriter("video.avi", fourcc, 20.0, (height, width))
-for i in range(1500):
-    
-    random_img = np.random.randint(255, size=(height, width, 3), dtype=np.uint8)
-    video.write(random_img)
-video.release()"""
-import cv2
-import numpy as np
- 
+s, _ = env.reset() 
 
-frameSize = (500, 500)
+height,width = env.render().shape[:2]
+frameSize = (width,height)
 
-out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 60, frameSize)
 
-for i in range(0,255):
-    img = np.ones((500, 500, 3), dtype=np.uint8)*i
-    out.write(img)
+
+
+out = cv2.VideoWriter('output/output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 60, frameSize)
+t_max = int(1e3)
+q_learning_agent = QLearningAgent(
+    learning_rate=0.5, epsilon=0.1, gamma=0.99, legal_actions=list(range(n_actions))
+)
+q_learning_eps_agent = QLearningAgentEpsScheduling(
+    learning_rate=0.5, epsilon=0.1, gamma=0.99, legal_actions=list(range(n_actions))
+)
+sarsa_agent = SarsaAgent(learning_rate=0.5, gamma=0.99, legal_actions=list(range(n_actions))) 
+
+agents = [q_learning_agent, q_learning_eps_agent, sarsa_agent]
+agent_names = ["Q-learning", "Q-learning with eps scheduling", "Sarsa"]
+for agent,name in zip(agents,agent_names):
+    print("Training agent: ", name)
+    train(agent, env, t_max, num_episodes=1000, recording=False)
+    print("Done")
+    print("Playing and recording agent: ", name)
+    s, _ = env.reset()            
+    for i in tqdm(range(0,t_max)):
+        action = agent.get_action(s)
+        next_s, r, done, _, _ = env.step(action)
+        s = next_s
+        
+        img = env.render()
+        out.write(img)
+        if done:
+            break
 
 out.release()
