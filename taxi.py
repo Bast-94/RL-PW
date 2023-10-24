c0b370588aa6527380f0ae5748d716be7bdb1697
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-24 20:43:44 +0000

    UPDATE(taxi.py):

--- taxi.py
+++ taxi.py
@@ -41,10 +41,30 @@ agent = QLearningAgent(
     learning_rate=0.5, epsilon=0.1, gamma=0.99, legal_actions=list(range(n_actions))
 )
 
+"""def run_episode(env: gym.Env, agent: QLearningAgent, t_max: int = int(1e4), train_mode:bool = True) -> float:
+    total_reward: t.SupportsFloat = 0.0
+    s, _ = env.reset()
 
+    for i in range(t_max):
+        # Get agent to pick action given state s
+        a = agent.get_action(s)
+
+        next_s, r, done, _, _ = env.step(a)
+
+        # Train agent for state s
+        # BEGIN SOLUTION
+
+        total_reward += r
+        if train_mode:
+            agent.update(state=s, action=a, next_state=next_s, reward=r)
+        s = next_s
+        if done:
+            break
+        # END SOLUTION
+
+    return total_reward"""
 def play_and_train(
-    env: gym.Env, agent: QLearningAgent, t_max=int(1e4), recording: bool = False
-) -> float:
+    env: gym.Env, agent: QLearningAgent, t_max=int(1e4)) -> float:
     """
     This function should
     - run a full game, actions given by agent.getAction(s)
