bd1f0c5d6eefc7033ebdb7f1ce1855b57e91718e
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-24 20:51:49 +0000

    UPDATE(video_maker.py): Different videos for every agent.

--- video_maker.py
+++ video_maker.py
@@ -17,19 +17,21 @@ frameSize = (width,height)
 
 
 
-out = cv2.VideoWriter('output/output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 60, frameSize)
+
 t_max = int(1e3)
 q_learning_agent = QLearningAgent(
     learning_rate=0.5, epsilon=0.1, gamma=0.99, legal_actions=list(range(n_actions))
 )
-q_learning_eps_agent = QLearningAgentEpsScheduling(
+eps_scheduling_agent = QLearningAgentEpsScheduling(
     learning_rate=0.5, epsilon=0.1, gamma=0.99, legal_actions=list(range(n_actions))
 )
 sarsa_agent = SarsaAgent(learning_rate=0.5, gamma=0.99, legal_actions=list(range(n_actions))) 
 
-agents = [q_learning_agent, q_learning_eps_agent, sarsa_agent]
-agent_names = ["Q-learning", "Q-learning with eps scheduling", "Sarsa"]
-for agent,name in zip(agents,agent_names):
+agents = [q_learning_agent, eps_scheduling_agent, sarsa_agent]
+agent_names = ["q_learning", "eps_scheduling", "sarsa"]
+
+outs = [ cv2.VideoWriter(f'{name}.avi',cv2.VideoWriter_fourcc(*'DIVX'), 60, frameSize) for name in agent_names]
+for agent,name,out in zip(agents,agent_names,outs):
     print("Training agent: ", name)
     train(agent, env, t_max, num_episodes=1000, recording=False)
     print("Done")
@@ -45,4 +47,4 @@ for agent,name in zip(agents,agent_names):
         if done:
             break
 
-out.release()
+    out.release()
