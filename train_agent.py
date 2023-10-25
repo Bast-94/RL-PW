84294be2586e986d83d48113ffcbdaa727c20f50
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-25 11:30:06 +0000

    UPDATE(train_agent.py): Fixing video making

--- train_agent.py
+++ train_agent.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import gymnasium as gym
 
 def train(agent:QLearningAgent, env:gym.Env, t_max:int = int(1e4), num_episodes:int = int(1e4), recording:bool = False):
-    for _ in tqdm(range(num_episodes)):
+    for _ in tqdm(range(num_episodes),desc="Training"):
         play_and_train(env, agent, t_max)
     return agent
 
