6422caadb9900969f3139715d75bd1bdf4c5a1a8
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-27 22:15:16 +0000

    UPDATE(train_agent.py): Reformat

--- train_agent.py
+++ train_agent.py
@@ -1,10 +1,17 @@
-from taxi import play_and_train
-from qlearning import QLearningAgent
-from tqdm import tqdm
 import gymnasium as gym
+from tqdm import tqdm
 
-def train(agent:QLearningAgent, env:gym.Env, t_max:int = int(1e4), num_episodes:int = int(1e4), recording:bool = False):
-    for _ in tqdm(range(num_episodes),desc="Training"):
+from qlearning import QLearningAgent
+from taxi import play_and_train
+
+
+def train(
+    agent: QLearningAgent,
+    env: gym.Env,
+    t_max: int = int(1e4),
+    num_episodes: int = int(1e4),
+    recording: bool = False,
+):
+    for _ in tqdm(range(num_episodes), desc="Training"):
         play_and_train(env, agent, t_max)
     return agent
-
