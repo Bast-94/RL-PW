91f4ed9ff393499ecf4c71735c47af9d4cf75854
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-28 13:04:25 +0000

    UPDATE(video_maker.py): Remove train_step and ep_step, trainning will be outside of this function.

--- video_maker.py
+++ video_maker.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import gymnasium as gym
 import imageio
@@ -6,38 +8,33 @@ from tqdm import tqdm
 
 
 def create_gif(
-    agent, name, ep_per_step, nb_step, artifact_dir, t_max, env, policy=None
+    agent,
+    env,
+    name,
+    artifact_dir=".",
+    t_max=int(1e4),
 ):
-    from train_agent import train
-
-    # gif_writer = imageio.get_writer(f"{artifact_dir}/{name}.gif", mode="I")
     frames = []
-    for train_step in tqdm(range(1, nb_step + 1)):
-        print(train_step)
-        train(agent, env, t_max, num_episodes=ep_per_step)
-        print("Done")
-        print("Playing and recording agent: ", name)
 
-        s, _ = env.reset()
-        for i in tqdm(range(0, t_max)):
-            action = agent.get_action(s)
-            next_s, r, done, _, _ = env.step(action)
-            s = next_s
+    s, _ = env.reset()
+    for i in tqdm(range(0, t_max)):
+        action = agent.get_action(s)
+        next_s, r, done, _, _ = env.step(action)
+        s = next_s
 
-            img = env.render()
-            img = cv2.putText(
-                img,
-                text=f"{name} agent after {train_step*ep_per_step} episodes \n {i} steps \n reward: {r}",
-                org=(10, 30),
-                fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                fontScale=1,
-                color=(255, 255, 255),
-                thickness=1,
-                lineType=cv2.LINE_AA,
-            )
-            frames.append(img)
-            # gif_writer.append_data(img)
-            if done:
-                break
-    imageio.mimsave(f"{artifact_dir}/{name}.gif", frames, fps=12,loop=0)
-    # gif_writer.close()
+        img = env.render()
+        img = cv2.putText(
+            img,
+            text=f"{name} agent, {i} steps, reward: {r}",
+            org=(10, 30),
+            fontFace=cv2.FONT_HERSHEY_DUPLEX,
+            fontScale=1,
+            color=(255, 255, 255),
+            thickness=1,
+            lineType=cv2.LINE_AA,
+        )
+        frames.append(img)
+        if done:
+            break
+    output_file_path = os.path.join(artifact_dir, f"{name}.gif")
+    imageio.mimsave(output_file_path, frames, fps=8, loop=0)
