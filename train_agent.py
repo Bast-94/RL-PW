a872be381799e4ae6bb931bcd4f3da7ae32015a3
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-28 14:28:28 +0000

    UPDATE(train_agent.py): Remove unuseful imports.

--- train_agent.py
+++ train_agent.py
@@ -2,15 +2,15 @@ import gymnasium as gym
 from tqdm import tqdm
 
 from qlearning import QLearningAgent
-from taxi import play_and_train
+from sarsa import SarsaAgent
 
 
 def train(
-    agent: QLearningAgent,
+    agent: QLearningAgent | SarsaAgent,
     env: gym.Env,
     t_max: int = int(1e4),
     num_episodes: int = int(1e4),
 ):
-    for _ in tqdm(range(num_episodes), desc="Training"):
+    for _ in tqdm(range(num_episodes)):
         agent.play_and_train(env, t_max)
     return agent
