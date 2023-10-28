3a48e13c916f6abe2b87bfcebdbc982077e93fe2
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-28 08:00:09 +0000

    UPDATE(video_maker.py): Refarctor in order to move video_making in taxi.py.

--- video_maker.py
+++ video_maker.py
@@ -4,39 +4,12 @@ import imageio
 import numpy as np
 from tqdm import tqdm
 
-from qlearning import QLearningAgent
-from qlearning_eps_scheduling import QLearningAgentEpsScheduling
-from sarsa import SarsaAgent
-from train_agent import train
 
-env = gym.make("Taxi-v3", render_mode="rgb_array")
-n_actions = env.action_space.n
-s, _ = env.reset()
+def create_gif(
+    agent, name, ep_per_step, nb_step, artifact_dir, t_max, env, policy=None
+):
+    from train_agent import train
 
-height, width = env.render().shape[:2]
-frameSize = (width, height)
-
-
-t_max = int(1e3)
-q_learning_agent = QLearningAgent(
-    learning_rate=0.5, epsilon=0.1, gamma=0.99, legal_actions=list(range(n_actions))
-)
-eps_scheduling_agent = QLearningAgentEpsScheduling(
-    learning_rate=0.5, epsilon=0.1, gamma=0.99, legal_actions=list(range(n_actions))
-)
-sarsa_agent = SarsaAgent(
-    learning_rate=0.5, gamma=0.99, legal_actions=list(range(n_actions))
-)
-
-agents = [q_learning_agent, eps_scheduling_agent, sarsa_agent]
-agent_names = ["q_learning", "eps_scheduling", "sarsa"]
-fps = 16
-nb_step = 10
-artifact_dir = "artifacts"
-ep_per_step = 100
-
-
-def create_gif(agent, name, ep_per_step, nb_step, artifact_dir):
     gif_writer = imageio.get_writer(f"{artifact_dir}/{name}.gif", mode="I")
     for train_step in tqdm(range(1, nb_step + 1)):
         print(train_step)
@@ -53,13 +26,13 @@ def create_gif(agent, name, ep_per_step, nb_step, artifact_dir):
             img = env.render()
             img = cv2.putText(
                 img,
-                f"Agent: {name} after {train_step*ep_per_step} episodes",
-                (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
+                text=f"{name} agent after {train_step*ep_per_step} episodes \n {i} steps \n reward: {r}",
+                org=(10, 30),
+                fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                fontScale=1,
+                color=(255, 255, 255),
+                thickness=1,
+                lineType=cv2.LINE_AA,
             )
 
             gif_writer.append_data(img)
@@ -67,7 +40,3 @@ def create_gif(agent, name, ep_per_step, nb_step, artifact_dir):
                 break
 
     gif_writer.close()
-
-
-for agent, name in zip(agents, agent_names):
-    create_gif(agent, name, 1000, 1, artifact_dir)
