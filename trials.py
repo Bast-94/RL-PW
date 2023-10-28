e196b37d96b9f2343cf2a7ae433c4a42cb545960
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-28 15:42:11 +0000

    UPDATE(trials.py):

--- trials.py
+++ trials.py
@@ -1,27 +1,17 @@
-import argparse
-
 import gymnasium as gym
-import matplotlib.pyplot as plt
 import numpy as np
-from tqdm import tqdm
 
-from qlearning import QLearningAgent
 from sarsa import SarsaAgent
-from taxi import play_and_train
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--epochs", type=int, default=1000)
-parser.add_argument("-v", "--verbose", action="store_true")
-parser.add_argument("-i", "--img_output_file", type=str, default=None)
-img_output_file = parser.parse_args().img_output_file
-verbose = parser.parse_args().verbose
-epochs = parser.parse_args().epochs
+# create random array of 5 integers between 0 and 10
+
 env = gym.make("Taxi-v3", render_mode="rgb_array")
-n_actions = env.action_space.n  # type: ignore
-s, _ = env.reset()
-env.step(0)
-plt.imshow(env.render())
-plt.savefig("taxi.png")
-env.step(1)
-plt.imshow(env.render())
-plt.savefig("taxi1.png")
+n_actions = env.action_space.n
+sarsa = SarsaAgent(
+    learning_rate=0.5,
+    gamma=0.99,
+    legal_actions=list(range(n_actions)),
+    policy="softmax",
+)
+state = env.reset()
+sarsa.play_and_train(env, t_max=5)
