3191a289b01881afb3a3d4299df18cb3912a7eb9
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-10-28 14:28:41 +0000

    UPDATE(video_maker.py): Fix truncated condition.

--- video_maker.py
+++ video_maker.py
@@ -7,33 +7,28 @@ import numpy as np
 from tqdm import tqdm
 
 
-def create_gif(
-    agent,
-    env,
-    name,
-    artifact_dir=".",
-    t_max=int(1e4),
-):
+def create_gif(agent, env, name, artifact_dir=".", t_max=int(1e4), text=""):
     frames = []
 
     s, _ = env.reset()
     for i in tqdm(range(0, t_max)):
         action = agent.get_action(s)
-        next_s, r, done, _, _ = env.step(action)
+        next_s, r, terminated, truncated, _ = env.step(action)
         s = next_s
-
+        done = terminated or truncated
         img = env.render()
         img = cv2.putText(
             img,
-            text=f"{name} agent, {i} steps, reward: {r}",
+            text=f"{name} agent, {i} steps, reward: {r}\n" + text,
             org=(10, 30),
             fontFace=cv2.FONT_HERSHEY_DUPLEX,
-            fontScale=1,
+            fontScale=0.5,
             color=(255, 255, 255),
             thickness=1,
             lineType=cv2.LINE_AA,
         )
         frames.append(img)
+        assert i <= 200, "Agent didn't finish the game in 200 steps"
         if done:
             break
     output_file_path = os.path.join(artifact_dir, f"{name}.gif")
